c834e74af21c8a1baad09cb21eb86f9625a03e2f
MarSteph <marley.stephens@gmail.com>
2025-06-22 16:00:47 -0500

    Rendered player to screen and added player rotation functionality

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 import pygame
+from player import Player
 from constants import *
 
 def main():
     pygame.init()
+    dt = 0
+    clock = pygame.time.Clock()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player = Player(x = SCREEN_WIDTH / 2, y = SCREEN_HEIGHT / 2)
     #pygame.display.set_caption("Hello Pygame")
     print("Starting Asteroids!")
     print("Screen width:", SCREEN_WIDTH)
@@ -15,9 +19,11 @@ def main():
                 return
 
         screen.fill((0,0,0))
+        player.update(dt)
+        player.draw(screen)
         pygame.display.flip()
-    
-    pygame.quit()
+
+        dt = clock.tick(60) / 1000
 
 if __name__=="__main__":
     main()
