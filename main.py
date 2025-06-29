570ca3720743a22b16e7410bd64e1e65ccfae6f1
MarSteph <marley.stephens@gmail.com>
2025-06-28 19:27:14 -0500

    Added split function to split asteroids when shots hit them

--- main.py
+++ main.py
@@ -37,6 +37,10 @@ def main():
             if asteroid.collision(player):
                 print("Game over!")
                 sys.exit()
+            for shot in shots:
+                if shot.collision(asteroid):
+                    shot.kill()
+                    asteroid.split()
         for item in drawable:
             item.draw(screen)
 
